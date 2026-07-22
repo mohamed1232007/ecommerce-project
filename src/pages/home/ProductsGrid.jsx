a816e8796fb6e359384bb38b6a6807a4d0f30d1a
mohamed1232007 <mohamedibraim1232007@gmail.com>
2026-07-22 08:21:12 +0300

    Initial commit for frontend React app

---
+++ src/pages/home/ProductsGrid.jsx
@@ -0,0 +1,19 @@
+import { Product } from "./product";
+
+export function ProductsGrid({ products, loadCart }) {
+    return (
+        <div className="home-page">
+            <div className="products-grid">
+                {products.map((product) => {
+                    return (
+                        <Product
+                            key={product.id}
+                            product={product}
+                            loadCart={loadCart}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
